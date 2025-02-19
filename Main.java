8b54ea141cf61f5957396c572e7d5e7dddd5791a
user <email>
2025-02-19 18:54:46 +0000

    thkkhjkhj

--- Main.java
+++ Main.java
@@ -7,10 +7,25 @@ public class Main
   {
     Scanner sc = new Scanner(System.in);
     String input = "";
+    ArrayList<String> theArrayList = new ArrayList<String>();
+    System.out.println("Type a word, type STOP to stop.");
 
     while (!input.equals("STOP"))
     {
-      System.out.println("infinite loop! replace with your code");
+      System.out.println("Inside loop. Enter a word");
+      input = sc.nextLine();
+      theArrayList.add(input);
+      System.out.println("Add the word. ArrayList is now " + theArrayList);
     }
+
+
+    theArrayList.remove(theArrayList.size()-1);
+    System.out.println("Exited loop. ArrayList is now " +\n + theArrayList.size() + \n + theArrayList);
+    theArrayList.set(theArrayList.size()-1, theArrayList.get(0));
+    if (theArrayList.size() > 2)
+    {
+    theArrayList.remove(0);
+    }
+    System.out.println(theArrayList);
   }
-}
+  }
